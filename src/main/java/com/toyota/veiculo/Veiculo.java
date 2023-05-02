c5659eca9fadabad47fd50e379418b0e1239369b
Mateus <maatmds04@gmail.com>
2023-05-02 18:58:46 -0300

    adding veichle to model

---
+++ src/main/java/com/toyota/veiculo/Veiculo.java
@@ -0,0 +1,26 @@
+package com.toyota.veiculo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract  class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_VEICULOS")
+    @SequenceGenerator(name = "SQ_VEICULOS", sequenceName = "SQ_VEICULOS")
+    @Column(name = "ID_VEICULO")
+    private Long id;
+
+    private String modelo;
+
+    private Date dataFabricacao;
+}
